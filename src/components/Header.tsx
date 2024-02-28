3ef6bd9fe8c1b43bd578eb155b48efa7f76c9ce4
Roland <rolandmartialkuate@gmail.com>
2024-02-28 05:56:09 +0100

    Updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,17 +2,18 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
-import { cn } from '@/lib/utils';
-import { Button } from './ui/button';
-import NavigationLinks from './NavigationLinks';
-import { Icons } from './Icons';
+import {Icons} from './Icons';
+import {useState} from "react";
 
 export default function Header() {
 
+    const [toggleMenu, setToggleMenu] = useState<boolean>(false)
+
     const onMenuToggle = (e) => {
         const navlinks = document.querySelector(".navLinks");
-        e.name = e.name === "menu" ? "close" : "menu";
-        navlinks.classList.toggle("left-[0%]");
+        setToggleMenu(!toggleMenu)
+        console.log(navlinks)
+        navlinks.classList.toggle("left-[-150%]")
     }
 
     return (
@@ -30,15 +31,44 @@ export default function Header() {
                     </Link>
                 </div>
                 <div className="flex items-center gap-3">
-                    <NavigationLinks className="hidden md:flex" />
+                    <div
+                        className="navLinks duration-500 absolute md:static md:w-auto w-full md:h-auto h-[85vh] bg-white flex md:items-center gap-[1.5vw] top-[100%] left-[0%] px-5 md:py-0 py-5 ">
+                        <ul className="flex md:flex-row flex-col md:items-center md:gap-[2vw] gap-8">
+                            <li className="relative max-w-fit pr-3 md:pr-0 py-1 after:bg-gradient-to-r from-[#AAB6867E] to-[#98C11F]  after:absolute after:h-1 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300">
+                                <a href="#">À propos de nous</a>
+                            </li>
+                            <li className="relative max-w-fit pr-3 md:pr-0 py-1 after:bg-gradient-to-r from-[#AAB6867E] to-[#98C11F]  after:absolute after:h-1 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300">
+                                <a href="#">Comment ça marche</a>
+                            </li>
+                            <li className="relative max-w-fit pr-3 md:pr-0 py-1 after:bg-gradient-to-r from-[#AAB6867E] to-[#98C11F]  after:absolute after:h-1 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300">
+                                <a href="#">Services</a>
+                            </li>
+                            <li className="relative max-w-fit pr-3 md:pr-0 py-1 after:bg-gradient-to-r from-[#AAB6867E] to-[#98C11F]  after:absolute after:h-1 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300">
+                                <a href="#">Tarifs</a>
+                            </li>
+                            <li className="relative max-w-fit pr-3 md:pr-0 py-1 after:bg-gradient-to-r from-[#AAB6867E] to-[#98C11F]  after:absolute after:h-1 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300">
+                                <a href="#">FAQ</a>
+                            </li>
+                        </ul>
+                    </div>
                     <div className="flex items-center gap-2">
                         <button
                             type="button"
-                            className="hover:bg-clip-text hover:text-transparent bg-gradient-to-br from-[#AAB6867E] to-[#98C11F] border-solid border-2 border-[#98C11F]  font-bold text-white px-5 py-2 rounded-full "
+                            className="hover:bg-clip-text hover:text-transparent bg-gradient-to-br from-[#AAB6867E] to-[#98C11F] border-solid border-2 border-[#98C11F]  font-bold text-white lg:px-5 lg:py-2 rounded-full md:text-sm text-xs p-2"
                         >
                             Contactez nous
                         </button>
-                        <Icons.whatsapp className="w-6 h-6" />
+                        {toggleMenu ? <Icons.menu
+                                name={'menu'}
+                                onClick={(e) => onMenuToggle(e)}
+                                className="w-6 h-6 text-[30px] cursor-pointer md:hidden"
+                            /> :
+                            <Icons.x
+                                name={'menu'}
+                                onClick={(e) => onMenuToggle(e)}
+                                className="w-6 h-6 text-[30px] cursor-pointer md:hidden"
+                            />
+                        }
                     </div>
                 </div>
             </nav>
